dee56b1e8f50a03fe2f477115361fd8b45b9f7c9
Administrator <1317660141@qq.com>
2019-10-22 00:51:32 +0800

    first

--- ly-common/src/main/java/com/leyou/common/enums/ExceptionEnums.java
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnums.java
@@ -15,6 +15,7 @@ public enum  ExceptionEnums {
     PRICE_CANNOT_BE_NULL(400,"价格不能为空"),
     CATEGORY_NOT_FOUND(404,"商品分类未找到"),
     BRAND_SAVE_ERROR(500,"新增品牌失败"),
+    UPLOAD_FILE_ERROR(500,"上传文件失败"),
 
 
 
